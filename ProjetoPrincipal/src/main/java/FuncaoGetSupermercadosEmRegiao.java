f0c178fa09d7274d2cf73c4797a439569cfdb3d3
DiegoSoler <diego.giliolli.santos@usp.br>
2018-12-11 13:48:01 -0200

    Esqueleto da função padrão pronta

--- ProjetoPrincipal/src/main/java/FuncaoGetSupermercadosEmRegiao.java
+++ ProjetoPrincipal/src/main/java/FuncaoGetSupermercadosEmRegiao.java
@@ -1,11 +1,11 @@
-
-        import com.amazonaws.services.lambda.runtime.Context;
-        import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class FuncaoGetSupermercadosEmRegiao implements RequestHandler<RequestClass, ResponseClass>{
 
     public ResponseClass handleRequest(RequestClass request, Context context){
-        String greetingString = String.format("Hello %s, %s.", request.firstName, request.lastName);
-        return new ResponseClass(greetingString);
+
+        
+        return new ResponseClass();
     }
 }
